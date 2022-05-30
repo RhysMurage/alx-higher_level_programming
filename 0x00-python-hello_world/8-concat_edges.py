2fbd0a96814e4058570d25149e78c3049ae34214
RhysMurage <rhysmwangi96@gmail.com>
2022-05-30 15:51:25 -0400

    Fix error

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
 str = (f"{str[39:67]}" f"{str[-22:-17]}" f"{str[0:6]}")
 print(str)
