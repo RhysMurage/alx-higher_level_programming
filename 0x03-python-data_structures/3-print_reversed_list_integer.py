79295e48bfb471824a06c934c57c6d9e469497b3
RhysMurage <rhysmwangi96@gmail.com>
2022-06-05 04:43:51 -0400

    Use list methods

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    end = len(my_list)-1
-    while end >= 0:
-        print("{}".format(my_list[end]))
-        end -= 1
+    my_list.reverse()
+    for i in my_list:
+        print("{}".format(i))
