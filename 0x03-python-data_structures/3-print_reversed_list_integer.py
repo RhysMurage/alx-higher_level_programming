4453db2752926580e883d861fb453c39dfe7024d
RhysMurage <rhysmwangi96@gmail.com>
2022-06-06 08:46:50 -0400

    Fix error for checker 7

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,6 +2,5 @@
 def print_reversed_list_integer(my_list=[]):
     if my_list:
         my_list.reverse()
-    for i in range(len(my_list)):
-        print("{:d}".format(my_list[i]))
-    return 0
+        for i in range(len(my_list)):
+            print("{:d}".format(my_list[i]))
