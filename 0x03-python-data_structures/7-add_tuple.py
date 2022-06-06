57c76e07550f0f674ab69864985da24bb2b18f86
RhysMurage <rhysmwangi96@gmail.com>
2022-06-06 08:50:49 -0400

    Fix error for checker 9

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    results = []
-    if len(tuple_a) == 0:
+    if tuple_a or tuple_b:
+        results = []
+        if len(tuple_a) == 0:
+            tuple_a = tuple_a[:2]
+            return tuple_b
+        if len(tuple_b) == 0:
+            tuple_b = tuple_b[:2]
+            return tuple_a
+        if len(tuple_a) == 1:
+            tuple_a = (tuple_a[0], 0)
+        if len(tuple_b) == 1:
+            tuple_b = (tuple_b[0], 0)
         tuple_a = tuple_a[:2]
-        return tuple_b
-    if len(tuple_b) == 0:
-        tuple_ab = tuple_b[:2]
-        return tuple_a
-    if len(tuple_a) == 1:
-        tuple_a = (tuple_a[0], 0)
-    if len(tuple_b) == 1:
-        tuple_b = (tuple_b[0], 0)
-    tuple_a = tuple_a[:2]
-    tuple_b = tuple_b[:2]
-    for x, y in zip(tuple_a, tuple_b):
-        zipped = x+y
-        results.append(zipped)
-    return tuple(results)
+        tuple_b = tuple_b[:2]
+        for x, y in zip(tuple_a, tuple_b):
+            zipped = x+y
+            results.append(zipped)
+        return tuple(results)
