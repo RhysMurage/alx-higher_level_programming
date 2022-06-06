48e222cf282b069334402e9467730e95c6048bf8
RhysMurage <rhysmwangi96@gmail.com>
2022-06-06 08:03:34 -0400

    Add whitespaces in lines 8 and 10

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -5,8 +5,8 @@ def delete_at(my_list=[], idx=0):
     if my_list:
         new_list = my_list[:]
         my_list.clear()
-        for x in range(0,idx):
+        for x in range(0, idx):
             my_list.append(new_list[x])
-        for x in range(idx+1,len(new_list)):
+        for x in range(idx+1, len(new_list)):
             my_list.append(new_list[x])
         return my_list
