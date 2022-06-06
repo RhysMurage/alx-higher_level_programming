d979d8ff922f822e6fe6fe895ad7249a67ad033e
RhysMurage <rhysmwangi96@gmail.com>
2022-06-06 05:38:35 -0400

    Fix error

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence is None:
-        return(0, None)
-    else:
+    if sentence:
         return(len(sentence), sentence[0])
+    else:
+        return(0, None)
