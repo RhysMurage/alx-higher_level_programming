1532f1e577807a1d77d727354594b1f6bcffa798
RhysMurage <rhysmwangi96@gmail.com>
2022-06-21 13:45:27 -0400

    Add private instance attribute

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -17,4 +17,4 @@ class Square():
             raise TypeError('size must be an integer')
         if size < 0:
             raise ValueError('size must be >= 0')
-        pass
+        self.__size = size
