cb5516d515bb206dd63a7cd0d9fc18e048e05b84
RhysMurage <rhysmwangi96@gmail.com>
2022-06-21 21:25:26 -0400

    Fix error for checker 9

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -31,6 +31,10 @@ class Square():
         """
         set size of square
         """
+        if type(self.__size) != int:
+            raise TypeError('size must be an integer')
+        if self.__size < 0:
+            raise ValueError('size must be >= 0')
         self.__size = value
 
     def area(self):
