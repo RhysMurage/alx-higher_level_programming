b450196ff4851b20a781cc2093c21c84954f0aab
RhysMurage <rhysmwangi96@gmail.com>
2022-06-27 15:41:04 -0400

    Add if block to __init__

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -16,6 +16,14 @@ class Rectangle():
             width (int): length from side to side
             height (int): lenght from top to bottom
         """
+        if type(height) != int:
+            raise TypeError('height must be an integer')
+        if height < 0:
+            raise ValueError('height must be >= 0')
+        if type(width) != int:
+            raise TypeError('width must be an integer')
+        if width < 0:
+            raise ValueError('width must be >= 0')
         self.__height = height
         self.__width = width
 
@@ -47,7 +55,7 @@ class Rectangle():
 
     @height.setter
     def height(self, value):
-        if type(value) is not int:
+        if type(value) != int:
             raise TypeError('height must be an integer')
         if value < 0:
             raise ValueError('height must be >= 0')
@@ -55,7 +63,7 @@ class Rectangle():
 
     @width.setter
     def width(self, value):
-        if type(value) is not int:
+        if type(value) != int:
             raise TypeError('width must be an integer')
         if value < 0:
             raise ValueError('width must be >= 0')
