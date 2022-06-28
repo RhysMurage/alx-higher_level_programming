090ccbf9bb0e53fdaa00176a7d1d77fc8d8f4d91
RhysMurage <rhysmwangi96@gmail.com>
2022-06-28 09:04:50 -0400

    Initial commit

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""This is the "0-add_integer" module
+
+The 0-add_integer module supplies one function, add_integer()
+"""
+
+
+def add_integer(a, b=98):
+    """
+    Sums two integers a and b together. b defaults to 98
+    """
+    if type(a) not in [float, int]:
+        raise TypeError('a must be an integer')
+    if type(b) not in [float, int]:
+        raise TypeError('b must be an integer')
+    return int(round(a+b))
