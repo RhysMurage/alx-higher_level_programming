fadb5a3223b05b2d6aa9ab172e94ce559e5c5521
RhysMurage <rhysmwangi96@gmail.com>
2022-07-04 16:15:12 -0400

    Remove trailing whitespace on line 12

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -9,7 +9,7 @@ class BaseGeometry():
     def area(self):
         """Raise an exception"""
         raise Exception('area() is not implemented')
-    
+
     def integer_validator(self, name, value):
         """validates if value is an integer"""
         if type(value) != int:
