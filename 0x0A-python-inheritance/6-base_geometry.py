195689efdf2f3269b624d4a00b870de575c44f14
RhysMurage <rhysmwangi96@gmail.com>
2022-07-04 15:42:11 -0400

    Initial commit

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+This module contains a class BaseGeometry that raises an exception"""
+
+
+class BaseGeometry():
+    """Raises an exception"""
+    def area(self):
+        """Raise an exception"""
+        raise Exception('area is not implemented')
