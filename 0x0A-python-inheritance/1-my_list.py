7a722634fc5861980011cc6357fe98627fdc8ef3
RhysMurage <rhysmwangi96@gmail.com>
2022-07-04 13:40:13 -0400

    Fix error

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -8,13 +8,6 @@ class MyList(list):
     """Prints a sorted list in ascending order
 
     """
-    def __init__(self, list_item=[]):
-        """__init__ method
-
-        Args:
-            list_item: list to be sorted
-        """
-        self.list_item = list_item
 
     def print_sorted(self):
         """Prints a sorted list
