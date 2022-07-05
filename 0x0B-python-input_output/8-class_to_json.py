66b96347458f3496d785d10de46ca5b9ded3b360
RhysMurage <rhysmwangi96@gmail.com>
2022-07-05 17:27:07 -0400

    Initial commit

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Module that has a function that returns a dictionary with simple
+data structure for JSON serialization of an object
+"""
+import json
+
+
+def class_to_json(obj):
+    """Returns a dictionary with simple data structures
+    for JSON serialization of an object
+    """
+    return json.dumps(vars(obj))
