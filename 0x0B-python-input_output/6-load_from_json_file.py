e6a64de4fbe63799ec85f44434da90e381dc6884
RhysMurage <rhysmwangi96@gmail.com>
2022-07-05 16:27:34 -0400

    Change from json.dumps to json.loads

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -10,4 +10,4 @@ def load_from_json_file(filename):
     """Creates an object from a JSON file"""
     with open(filename, encoding='utf-8') as f:
         data = f.read()
-        return json.dumps(data)
+        return json.loads(data)
