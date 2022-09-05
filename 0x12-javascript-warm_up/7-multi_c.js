2326f899896d1f30d6b52d95a2898afb085ca320
RhysMurage <rhysmwangi96@gmail.com>
2022-09-05 10:26:37 -0400

    Initial commit

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+let i = 0;
+
+if (Number.isNaN(parseInt(process.argv[2]))) {
+  console.log('Missing number of occurrences');
+} else {
+  while (i < parseInt(process.argv[2])) {
+    console.log('C is fun');
+    i++;
+  }
+}
