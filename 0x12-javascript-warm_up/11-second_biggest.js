6d94b20eaef3b06305a86a3990a065e9232fb835
RhysMurage <rhysmwangi96@gmail.com>
2022-09-05 13:34:31 -0400

    Initial commit

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+if (process.argv.length <= 3) {
+  console.log(0);
+} else {
+  const newArray = process.argv.slice(2);
+  console.log(newArray.sort(function (a, b) { return b - a; })[1]);
+}
