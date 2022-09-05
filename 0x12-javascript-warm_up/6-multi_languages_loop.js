37ce3472b6186d8522e9b26737a0040c35d3d994
RhysMurage <rhysmwangi96@gmail.com>
2022-09-05 14:50:43 -0400

    Change Javascript to JavaScript

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const arrayString = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const arrayString = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (const item of arrayString) {
   console.log(item);
 }
