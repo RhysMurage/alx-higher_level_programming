3b4946bfca29be68a8b150ed3f23b822c235bae2
RhysMurage <rhysmwangi96@gmail.com>
2022-09-05 10:56:03 -0400

    Initial commit

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+const i = parseInt(process.argv[2]);
+if (Number.isNaN(i)) {
+  console.log('Missing size');
+} for (let len = 0; len < i; len++) {
+  console.log('X'.repeat(i));
+}
