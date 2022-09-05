56996dea587e4c38276b26c07cec1cda88bdcbf1
RhysMurage <rhysmwangi96@gmail.com>
2022-09-05 14:59:53 -0400

    Fix error for checker 9

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const process = require('process');
-
+/*
 const i = parseInt(process.argv[2]);
 function factorial (a) {
   if (a === 0) {
@@ -10,3 +10,19 @@ function factorial (a) {
 }
 
 console.log(factorial(i));
+*/
+
+let digit = Number(process.argv[2]);
+
+if (isNaN(digit)) {
+  digit = 1;
+}
+console.log(doFactorial(digit));
+
+function doFactorial (a) {
+  if (a === 0 || isNaN(a)) {
+    return (1);
+  }
+  const result = a * doFactorial(a - 1);
+  return (result);
+}
