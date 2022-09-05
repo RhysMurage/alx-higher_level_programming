dbf68deb2281f7fe3ca1e67968585a1c3c8a6505
RhysMurage <rhysmwangi96@gmail.com>
2022-09-05 11:59:16 -0400

    Initial commit

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const i = parseInt(process.argv[2]);
+
+function factorial (a) {
+  if (a === 0) {
+    return 1;
+  }
+  return a * factorial(a - 1);
+}
+
+console.log(factorial(i));
