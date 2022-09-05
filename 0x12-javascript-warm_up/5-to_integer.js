c2ea459f9c20291d6d90934d26e3c588254bd0f4
RhysMurage <rhysmwangi96@gmail.com>
2022-09-05 09:39:07 -0400

    Initial commit

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+if (process.argv.length === 2) {
+  console.log('Not a number');
+} else if (Number.isNaN(parseInt(process.argv[2]))) {
+  console.log('Not a number');
+} else {
+  console.log(parseInt(process.argv[2]));
+}
