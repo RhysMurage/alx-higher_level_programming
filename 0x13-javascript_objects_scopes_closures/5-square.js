81106f6ffead1f664ba9d4748dfe89cb48c807ea
RhysMurage <rhysmwangi96@gmail.com>
2022-09-06 18:09:49 -0400

    Fix error for checker 6

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -3,18 +3,7 @@ const Rectangle = require('./4-rectangle');
 
 class Square extends Rectangle {
   constructor (size) {
-    super();
-    this.size = size;
-  }
-
-  print () {
-    for (let i = 0; i < this.size; i++) {
-      console.log('X'.repeat(this.size));
-    }
-  }
-
-  double () {
-    this.size = this.size * 2;
+    super(size, size);
   }
 }
 
