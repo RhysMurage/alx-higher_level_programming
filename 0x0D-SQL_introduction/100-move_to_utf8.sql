ff9afc32544124e5e9ba578a783e0c81b9104309
RhysMurage <rhysmwangi96@gmail.com>
2022-09-13 16:55:13 -0400

    Fix error

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -4,5 +4,8 @@
 -- converts table to utf8 second
 -- converts a field in a table to utf8 last
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
--- ALTER TABLE hbtn_0c_0.first_table CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
--- ALTER TABLE hbtn_0c_0.first_table MODIFY `name` VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- a script that converts hbtn_0c_0 database to UTF8
+-- Changes the properties of a table in the database
+USE hbtn_0c_0;
+ALTER TABLE first_table.name
+   CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
