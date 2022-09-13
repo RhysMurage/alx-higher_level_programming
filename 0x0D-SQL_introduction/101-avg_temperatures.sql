ff9200ff16bc6f8b3a0b4211571773a49c726e53
RhysMurage <rhysmwangi96@gmail.com>
2022-09-13 16:09:35 -0400

    Fix error

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,6 +1,6 @@
 -- at displays the average temperature
 -- ordered by temperature descending
-SELECT AVG(value) AS avg_temp
+SELECT city, AVG(value) AS avg_temp
 	FROM temperatures
 	GROUP BY city
 	ORDER BY avg_temp DESC;
